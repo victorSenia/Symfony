0c68a83d7cc7931d6c3ffbd900b987f72f0441dd
Viktor Senchenko <viktor.senchenko@loyd.international>
2016-02-05 13:19:22 +0200

    security and cache

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -19,8 +19,45 @@ class ArticleController extends \Symfony\Bundle\FrameworkBundle\Controller\Contr
     /**
      * @Route("/article/{slug}", name="article_show")
      */
-    public function showAction($slug) {
+    public function showaAction($slug) {
 // ...
+    }
+
+    public function showAction(Article $article, Request $request) {
+        $author = $article->getAuthor();
+        $articleDate = new \DateTime($article->getUpdatedAt());
+        $authorDate = new \DateTime($author->getUpdatedAt());
+        $date = $authorDate > $articleDate ? $authorDate : $articleDate;
+        $response = new Response();
+        $response->setLastModified($date);
+// Set response as public. Otherwise it will be private by default.
+        $response->setPublic();
+
+        // set one vary header
+        $response->setVary('Accept-Encoding');
+// set multiple vary headers
+        $response->setVary(array('Accept-Encoding', 'User-Agent'));
+
+        if ($response->isNotModified($request)) {
+            return $response;
+        }
+
+        // Marks the Response stale
+        $response->expire();
+// Force the response to return a proper 304 response with no content
+        $response->setNotModified();
+        // Set cache settings in one call
+        $response->setCache(array(
+            'etag' => $etag,
+            'last_modified' => $date,
+            'max_age' => 10,
+            's_maxage' => 10,
+            'public' => true,
+// 'private' => true,
+        ));
+
+// ... do more work to populate the response with the full content
+        return $response;
     }
 
 }
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,17 +5,48 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
+    /**
+     * @Route("/admin")
+     */
+    public function adminAction() {
+        if
+        (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            throw $this->createAccessDeniedException();
+        }
+        $user = $this->getUser();
+// the above is a shortcut for this
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+
+        $response = new Response('<html><body>Admin page!</body></html>');
+// mark the response as either public or private
+        $response->setPublic();
+        $response->setPrivate();
+// set the private or shared max age
+        $response->setMaxAge(600);
+        $response->setSharedMaxAge(600);
+// set a custom Cache-Control directive
+        $response->headers->addCacheControlDirective('must-revalidate', true);
+        
+        return $response;
+    }
+
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
-    {
+    public function indexAction(Request $request) {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'Unable to access this page!');
+        // Old way :
+// if (false ===$this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+// throw $this->createAccessDeniedException('Unable to access this page!');
+// }
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+                    'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..'),
         ));
     }
+
 }
